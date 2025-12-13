01f4f3e6ff536708d08455f9ba85a274c05c7c75
Siva-Flutter-Dev <siva.flutter.dev@gmail.com>
2025-12-13 21:37:52 +0530

    14th commit

--- lib/audio_play_or_record.dart
+++ lib/audio_play_or_record.dart
@@ -1,6 +1,7 @@
 library audio_play_or_record;
 
 export 'src/models/audio_message_config.dart';
+export 'src/models/record_button_config.dart';
 export 'src/ui/audio_message_bubble.dart';
 export 'src/ui/record_mic_button.dart';
 export 'src/controllers/audio_record_controller.dart';
