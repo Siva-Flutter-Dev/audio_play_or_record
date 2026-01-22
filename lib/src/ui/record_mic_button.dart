6c68519556c1c23a2d9ced125ee5206228d4ee17
Siva-Flutter-Dev <siva.flutter.dev@gmail.com>
2026-01-22 20:00:41 +0530

    code revert

--- lib/src/ui/record_mic_button.dart
+++ lib/src/ui/record_mic_button.dart
@@ -36,9 +36,6 @@ class RecordMicButton extends StatefulWidget {
   /// Configuration for recording behavior, haptics, locking, and animations.
   final RecordButtonConfig config;
 
-  /// Mic button click finder
-  final Function(bool)? recordFinder;
-
   /// Callback executed when recording is finished.
   ///
   /// [path] is the path of the recorded audio file.
@@ -122,7 +119,6 @@ class RecordMicButton extends StatefulWidget {
     this.isSendEnable = false,
     this.height = 62,
     this.widgetSpacing,
-    this.recordFinder,
     this.config = const RecordButtonConfig(),
     this.overlayWidth,
     this.textController,
@@ -260,7 +256,6 @@ class _RecordMicButtonState extends State<RecordMicButton>
   Future<void> _tapStart() async {
     if (!widget.config.enableTapRecord || _state != RecordState.idle) return;
     await _startRecord(LongPressStartDetails(globalPosition: Offset.zero));
-    widget.recordFinder?.call(true);
   }
 
   /// Handles gesture updates for cancel or lock actions.
@@ -295,7 +290,6 @@ class _RecordMicButtonState extends State<RecordMicButton>
       setState(() => _state = RecordState.idle);
       widget.recordingState?.call(_state);
     }
-    widget.recordFinder?.call(false);
   }
 
   /// Cancels the recording and triggers the onDelete callback.
@@ -309,7 +303,6 @@ class _RecordMicButtonState extends State<RecordMicButton>
       _audioController = null;
       _state = RecordState.idle;
       widget.recordingState?.call(_state);
-      widget.recordFinder?.call(false);
     });
   }
 
